8c5f57e77dbc21592d51adb1d1bdd833d9a6e8d6
Tuyen2k <tuyen4835@gmail.com>
2023-11-06 21:56:55 +0700

    add lib axios, formik, firebase

---
+++ fe_md6/src/component/CreateProduct.js
@@ -0,0 +1,18 @@
+import {useState} from "react";
+
+export default function CreateProduct(){
+    const [product, setProduct] = useState({
+        name:"",
+        price:0,
+        image:"",
+        category:[],
+        timeMake:""
+    })
+
+
+    return(
+        <>
+
+        </>
+    )
+}
