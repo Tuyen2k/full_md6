040fb6ea1c3b41b2f56fd4769577fc240e15ff16
Tuyen2k <tuyen4835@gmail.com>
2023-11-06 11:18:16 +0700

    delete ward, district, citymapper

--- be_md6/src/main/java/com/example/du_an_md6/mapper/CityMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.du_an_md6.mapper;
-
-import com.example.du_an_md6.model.City;
-import com.example.du_an_md6.model.dto.CityDTO;
-
-
-public interface CityMapper extends EntityMapper<CityDTO, City> {
-}
--- be_md6/src/main/java/com/example/du_an_md6/mapper/DistrictMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.du_an_md6.mapper;
-
-import com.example.du_an_md6.model.District;
-import com.example.du_an_md6.model.dto.DistrictDTO;
-
-
-
-public interface DistrictMapper extends EntityMapper<DistrictDTO, District> {
-}
