da3a871d977fbe79405116cd7e32e6d2efa3d947
Tuyen2k <tuyen4835@gmail.com>
2023-11-07 22:10:20 +0700

    validate and notification modal success

--- fe_md6/src/component/CreateProduct.js
+++ fe_md6/src/component/CreateProduct.js
@@ -1,20 +1,23 @@
 import {useEffect, useRef, useState} from "react";
-import {Field, Form, Formik} from "formik";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import {saveProduct} from "../service/PruductService";
 import {getAllCategories} from "../service/CategoryService";
 import {upImageFirebase} from "../firebase/Upfirebase";
+import * as yup from "yup";
 
 export default function CreateProduct() {
     const [file, setFile] = useState(undefined)
     const [load, setLoad] = useState(true)
     const inputFile = useRef()
+    const btn_modal = useRef()
+    const [message, setMessage] = useState("")
     const [categoriesDB, setCategoriesDB] = useState([])
     const [categories, setCategories] = useState([])
     const [product, setProduct] = useState({
         name: "",
         price: "",
         image: "",
-        timeMake: ""
+        timeMake: "",
     })
 
     useEffect(() => {
@@ -24,19 +27,37 @@ export default function CreateProduct() {
     }, [])
 
     const handledCreate = (e) => {
+        if (file === undefined){
+            setMessage("Please choose image for the product!!!")
+            btn_modal.current.click();
+            return
+        }
+        if (categories === undefined || categories.length === 0){
+            setMessage("Please select category for the product!!!")
+            btn_modal.current.click();
+            return
+        }
         setLoad(false)
-        upImageFirebase(file).then(res =>{
-            let a = {id_merchant : 1}
-            let product ={...e, image : res.name, categories : categories, merchant:a, priceSale : e.price*0.95}
-            saveProduct(product).then(response =>{
+        upImageFirebase(file).then(res => {
+            let a = {id_merchant: 1}
+            let product = {...e, image: res.name, categories: categories, merchant: a, priceSale: e.price * 0.95}
+            saveProduct(product).then(response => {
                 setLoad(true)
                 setFile(undefined)
-                alert(response)
+                setMessage("Create product success!!!")
+                btn_modal.current.click();          // onclick btn modal
+            }).catch(Error =>{
+                console.log(Error)
+                setLoad(true)
+                setMessage("Action error occurred. Please check again!!!")
+                btn_modal.current.click();          // onclick btn modal
             })
-        }).catch(Error =>{
+        }).catch(Error => {
             setLoad(true)
-            alert("Error loading!!!")
-            console.log(Error)})
+            setMessage("Action error occurred. Please check again!!!")
+            btn_modal.current.click();          // onclick btn modal
+            console.log("up file"+Error)
+        })
     }
     const handledClickInput = () => {
         inputFile.current.click();
@@ -63,82 +84,124 @@ export default function CreateProduct() {
         setFile(file);
     }
 
+    const schema = yup.object().shape({
+        name: yup.string().required("Name is a data field that cannot be left blank."),
+        price: yup.number().required("Price is a data field that cannot be left blank.").min(5000,"Price cannot be lower than 5000."),
+        timeMake: yup.string().required("Time make is a data field that cannot be left blank.")
+    })
+
 
     return (
         <>
             {load ? (
-            <div className="form-input">
-                <h1 className="title">Create Product</h1>
-                <Formik onSubmit={(e) => handledCreate(e)}
-                        initialValues={product}>
-                    <Form>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="name">Name</span>
-                            <Field type="text" className="form-control" name="name" placeholder="Enter name product"
-                                   aria-describedby="name"/>
-                        </div>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="price">Price</span>
-                            <Field type="text" className="form-control" name="price" placeholder="Enter price product"
-                                   aria-describedby="price"/>
-                        </div>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="timeMake">Time Make</span>
-                            <Field type="text" className="form-control" name="timeMake"
-                                   placeholder="Enter time make product"
-                                   aria-describedby="timeMake"/>
-                        </div>
-                        <div style={{display: "flex"}} className="div-checkbox input-group mb-3 row ">
-                            <span className="input-group-text col-2" style={{height: "37.6px"}}>Categories</span>
-                            <div className="form-checkbox col-10">
-                                {categoriesDB.map((category, index = 0) => {
-                                    return (
-                                        <div className="form-check" key={index}>
-                                            <input className="form-check-input" type="checkbox"
-                                                   onChange={(e) => handledCategories(e.target.value)}
-                                                   value={category.id_category} id={"categories" + index}/>
-                                            <label className="form-check-label"
-                                                   htmlFor={"categories" + index}>{category.name}</label>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                        <div className="mb-3" style={{display: "flex"}} onClick={handledClickInput}>
-                            <span className="input-group-text" style={{height: "37.6px"}}>Image Product</span>
-                            <input ref={inputFile} className="form-control" type="file" id="formFile"
-                                   style={{display: 'none'}} onChange={(e) => handledInputFile(e.target.files[0])}/>
-                            {file === undefined ? (
-                                <div style={{backgroundColor: "white", width: "325.6px", marginLeft: "20px"}}
-                                     className="form-control">
-                                    <div className='image-input'
-                                         style={{backgroundImage: `url("https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png")`}}>
+                    <div className="form-input">
+                        <h1 className="title">Create Product</h1>
+                        <Formik onSubmit={(e) => handledCreate(e)}
+                                initialValues={product} validationSchema={schema}>
+                            <Form>
+
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text" id="name">Name</span>
+                                    <Field type="text" className="form-control" name="name" placeholder="Enter name product"
+                                           aria-describedby="name"/>
+                                    <ErrorMessage name="name" component="span" className="error"/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text" id="price">Price</span>
+                                    <Field type="text" className="form-control" name="price"
+                                           placeholder="Enter price product"
+                                           aria-describedby="price"/>
+                                    <ErrorMessage name="price" component="span" className="error"/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text" id="timeMake">Time Make</span>
+                                    <Field type="text" className="form-control" name="timeMake"
+                                           placeholder="Enter time make product"
+                                           aria-describedby="timeMake"/>
+                                    <ErrorMessage name="timeMake" component="span" className="error"/>
+                                </div>
+                                <div style={{display: "flex"}} className="div-checkbox input-group mb-3 row ">
+                                    <span className="input-group-text col-2" style={{height: "37.6px"}}>Categories</span>
+                                    <div className="form-checkbox col-10">
+                                        {categoriesDB.map((category, index = 0) => {
+                                            return (
+                                                <div className="form-check" key={index}>
+                                                    <input className="form-check-input" type="checkbox"
+                                                           onChange={(e) => handledCategories(e.target.value)}
+                                                           value={category.id_category} id={"categories" + index}/>
+                                                    <label className="form-check-label"
+                                                           htmlFor={"categories" + index}>{category.name}</label>
+                                                </div>
+                                            )
+                                        })}
                                     </div>
                                 </div>
-                            ) : (
-                                <div style={{backgroundColor: "white", width: "325.6px", marginLeft: "20px"}}
-                                     className="form-control">
-                                    <div>
-                                        <img className="image-input" src={URL.createObjectURL(file)}
-                                             alt='image'/>
+                                <div className="mb-3" style={{display: "flex"}} onClick={handledClickInput}>
+                                    <span className="input-group-text" style={{height: "37.6px"}}>Image Product</span>
+                                    <input ref={inputFile} className="form-control" name="image" type="file" id="formFile"
+                                           style={{display: 'none'}} onChange={(e) => handledInputFile(e.target.files[0])}/>
+                                    {file === undefined ? (
+                                        <div style={{backgroundColor: "white", width: "325.6px", marginLeft: "20px"}}
+                                             className="form-control">
+                                            <div className='image-input'
+                                                 style={{backgroundImage: `url("https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png")`}}>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <div style={{backgroundColor: "white", width: "325.6px", marginLeft: "20px"}}
+                                             className="form-control">
+                                            <div>
+                                                <img className="image-input" src={URL.createObjectURL(file)}
+                                                     alt='image'/>
+                                            </div>
+                                        </div>)}
+                                    {/*<ErrorMessage name="image" component="span" className="error"/>*/}
+                                </div>
+                                <hr/>
+                                <div className="div-button">
+                                    <button className="btn btn-outline-primary" type={"submit"}>Save</button>
+                                    <button className="btn btn-outline-primary" type={"button"}>Back Home</button>
+                                </div>
+                            </Form>
+                        </Formik>
+
+                        {/*button modal*/}
+                        <button type="button" ref={btn_modal} className="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal" style={{display: "none"}}>
+                        </button>
+
+                        {/*modal*/}
+                        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
+                             aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="exampleModalLabel">Notification</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
                                     </div>
-                                </div>)}
+                                    <div className="modal-body">
+                                        <span>{message}</span>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <hr/>
-                        <div className="div-button">
-                            <button className="btn btn-outline-primary" type={"submit"}>Save</button>
-                            <button className="btn btn-outline-primary" type={"button"}>Back Home</button>
-                        </div>
-                    </Form>
-                </Formik>
-            </div> )
-                :(
+                    </div>
+                )
+                : (
                     <div className="d-flex justify-content-center">
-                        <div className="spinner-border" style={{width: "4rem", height: "4rem", marginTop: "40vh"}} role="status">
+                        <div className="spinner-border" style={{width: "4rem", height: "4rem", marginTop: "40vh"}}
+                             role="status">
                             <span className="visually-hidden">Loading...</span>
                         </div>
                     </div>
                 )}
+
+
         </>
     )
 }
